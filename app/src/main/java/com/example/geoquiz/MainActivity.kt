c0bf07e45a102a166a3b5723950a02544603c2c6
David Watkins <davidwatkins@Davids-MacBook-Pro.local>
2020-01-15 23:02:10 -0500

    starting repo to store practice online

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate(Bundle?) called")
+
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.previous_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+//        val trueToast = Toast.makeText(this,R.string.correct_toast, Toast.LENGTH_SHORT)
+//        trueToast.setGravity(Gravity.TOP, 0, 250)
+//        val falseToast = Toast.makeText(this,R.string.incorrect_toast, Toast.LENGTH_SHORT)
+//        falseToast.setGravity(Gravity.TOP, 0, 250)
+        trueButton.setOnClickListener {
+            checkAnswer(true)
+            trueButton.isClickable = false}
+        falseButton.setOnClickListener {
+            checkAnswer(false)
+            }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+//            val questionTextResId = questionBank[currentIndex].textResId
+//            questionTextView.setText(questionTextResId)
+        }
+        prevButton.setOnClickListener{
+            currentIndex = (currentIndex - 1) % questionBank.size
+            if (currentIndex == -1)
+                currentIndex = questionBank.size - 1
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+//        val questionTextResId = questionBank[currentIndex].textResId
+//        questionTextView.setText(questionTextResId)
+        updateQuestion()
+    }// on create
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+
+}
