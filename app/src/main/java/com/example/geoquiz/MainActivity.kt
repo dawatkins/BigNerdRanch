66e089fd22c599ad9bd618b46a24c140ab0dc5bb
David Watkins <davidwatkins@Davids-MacBook-Pro.local>
2020-02-20 16:35:34 -0500

    finish chapter 7

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.geoquiz
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.ActivityOptions
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -25,11 +29,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var questionTextView: TextView
     private lateinit var cheatButton: Button
     private var totalCorrect = 0.0
+    private lateinit var usersAPILevel: TextView
 
     private val quizViewModel: QuizViewModel by lazy{
         of(this).get(QuizViewModel::class.java)
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -46,6 +52,7 @@ class MainActivity : AppCompatActivity() {
         prevButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
         cheatButton = findViewById(R.id.cheat_button)
+        usersAPILevel = findViewById(R.id.api_level)
 
         trueButton.setOnClickListener {
             checkAnswer(true)
@@ -82,12 +89,18 @@ class MainActivity : AppCompatActivity() {
 //            updateQuestion()
 //        }
 
-        cheatButton.setOnClickListener{
+        cheatButton.setOnClickListener{ view ->
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val options =
+                    ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.width, view.height)
+                startActivityForResult(intent, REQUEST_CODE_CHEAT, options.toBundle())
+            } else {
+                startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            }
 //            val intent = Intent(this, CheatActivity::class.java)
 //            startActivity(intent)
-            startActivityForResult(intent, REQUEST_CODE_CHEAT)
         }
 
         updateQuestion()
